77123e0824b44a443840cf5c1c8b8032553fd867
AliBahgatAliMohamed <93349210+AliBahgatAliMohamed@users.noreply.github.com>
2021-10-29 23:17:39 +0200

    Add files via upload

---
+++ assigment.m
@@ -0,0 +1,32 @@
+%% Task1 
+% Name : Ali Bahgat Ali mohamed
+% Group :B1
+%% matrices
+A=[-7 5 -9; 2 -1 2; 1 -1 2]
+B=[16 3 2 13;5 10 11 8;9 6 7 12; 4 15 14 1]
+D=[6 3 2; 2 12 -7; -1 6 2; -5 15 11]
+C=[4 2 -3;7 -7 9; 3 -5 6]
+%% point 1
+ans1=3.*A-5.*C
+% ans2=7.*A+2.*B 
+% eror
+ans3=C*A
+ans4=C*D'
+%% point 2
+zeros(3)
+zeros (3,4)
+ones (3)
+ones (3,4)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(3)
+%% point 3
+%error
+%% point 4
+W= diag([5 5 5 5 5 5 5])
+W(:,8)=(5)
+%% point 5
+
+A(1,:)
+A(:,2)
